37f8f9df25ed51c2712a0899814cfb559e9bbbce
DZamora54 <dantedomenic@hotmail.com>
2017-09-19 19:51:19 -0400

    phase III back end

--- server.js.js
+++ server.js.js
@@ -12,8 +12,50 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
+var reservations = [{
+  name: "",
+  phoneNumber: "",
+  email: "",
+  uniqueId: "",
+}];
+
+//Routes
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+app.get("/tables", function(req, res) {
+  res.json(reservations);
+});
+
+// Search existing reservations
+app.get("/api/:reservations?", function(req, res) {
+  var chosen = req.params.reservations;
+  if (chosen) {
+  	console.log(chosen);
+  	for (var i = 0; i < reservations.length; i++) {
+  	  if (chosen === reservations[i].routeName) {
+  	  	return res.json(reservations[i]);
+  	  }
+  	}
+  	return res.json(false);
+  }
+  return res.json(reservations);
+});
+
+// Create new reservation
+app.post("/api/new", function(req, res) {
+  var newreservation = req.body;
+  newreservation.routeName = newreservation.name.replace(/\s+/g, "").toLowerCase();
+  console.log(newreservation);
+  reservations.push(newreservation);
+  res.json(newreservation);
+});
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
-});	
+});
